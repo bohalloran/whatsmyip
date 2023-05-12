6b50af7ba8638c41939c7d3b3a6f148b6d688357
bohalloran <brian@ohalloran.com>
2023-05-12 14:56:35 -0400

    add some new code

---
+++ docker-httpd-image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# TODO: review and see what we should put in simmplisafe managed AWS registry/artifactory account
+#
+# update and get the creds
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \``
+    gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+# download and install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# get and run whatsmyip
+sudo docker run -dit --name whatsmyip -p 8080:8080 mdevey/whatsmyip
---
+++ main.tf
@@ -0,0 +1,85 @@
+// TODO: add terraform for creating VPC/subnet, for now create manually
+// TODO: add certificate, route53, ssl
+variable "awsprops" {
+    type = map
+    default = {
+    region = "us-east-1"
+    vpc = "vpc-0839f3a73a5393327"
+    ami = "ami-007855ac798b5175e"
+    itype = "t2.micro"
+    subnet = "subnet-0e13b91c69279bb55"
+    publicip = true
+    keyname = "whatsmyip"
+    secgroupname = "whatsmyip-sg"
+  }
+}
+
+provider "aws" {
+  region = lookup(var.awsprops, "region")
+  profile = "bohalloran"
+}
+
+resource "aws_security_group" "whatsmyip-sg-ref" {
+  name = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id = lookup(var.awsprops, "vpc")
+
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 8080 Transport
+  ingress {
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = false
+  }
+}
+
+
+resource "aws_instance" "whatsmyip-instance-ref" {
+  ami = lookup(var.awsprops, "ami")
+  instance_type = lookup(var.awsprops, "itype")
+  subnet_id = lookup(var.awsprops, "subnet")
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name = lookup(var.awsprops, "keyname")
+  user_data = file("docker-httpd-image.sh")
+
+
+  vpc_security_group_ids = [
+    aws_security_group.whatsmyip-sg-ref.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    volume_size = 50
+    volume_type = "gp2"
+  }
+  tags = {
+    Name ="whatsmyip server"
+    Environment = "dev"
+    OS = "ubuntu"
+  }
+
+  depends_on = [ aws_security_group.whatsmyip-sg-ref ]
+}
+
+
+output "external-ip"{
+  value = aws_instance.whatsmyip-instance-ref.public_ip
+}
